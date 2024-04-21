8f2de1ab47c26bded71ea6ea1392a51f9eb8c77b
Prajeshzzzzz <prajeshram14@gmail.com>
2024-04-21 09:57:43 +0530

    First commit

---
+++ C37_CoreJava/src/com/tns/dayfour/Executor1.java
@@ -0,0 +1,11 @@
+package com.tns.dayfour;
+import com.tns.dayfour.Base;
+public class Executor1 {
+public static void main(String[] args) {
+Base b1 = new Base();
+
+b1.methodPublic();
+b1.varPublic = 41;
+b1.methodPublic();
+}
+}
