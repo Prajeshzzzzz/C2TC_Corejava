1a6e0e1f0867f0844ad51f7997cf3ef85a1aa55e
Prajeshzzzzz <prajeshram14@gmail.com>
2024-04-21 10:33:25 +0530

    First commit

---
+++ C37_CoreJava/src/com/tns/dayfive/SLInheritanceDemo.java
@@ -0,0 +1,8 @@
+package com.tns.dayfive;
+
+public class SLInheritanceDemo {
+	public static void main(String[] args) {
+		Student1 student = new Student1("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+		System.out.println(student);
+	}
+}
